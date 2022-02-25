87fa00a6c66a512cbb02395fa9aa32e0301dfb29
edisonthk <edisonthk@gmail.com>
2022-02-25 22:02:17 +0900

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <>
+    <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+}
+
+export default MyApp
